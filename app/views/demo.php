ac4ee750cd8a65089539279dbcf18ee515152822
Alexander Bykhov <another.side.of.my.soul@gmail.com>
2014-02-01 00:36:59 +0400

    Implement pretty demo design into the PHP demo

--- app/views/demo.php
+++ app/views/demo.php
@@ -1,26 +1,65 @@
 <!DOCTYPE html>
 <html>
     <head>
+        <title>visualCaptcha - A cool visual drag-and-drop captcha jQuery plugin by emotionLoop - Demo</title>
         <meta charset="utf-8">
-        <title>visualCaptcha Demo</title>
+        <meta name="keywords" content="visualcaptcha, visual, jquery captcha, captcha, mobile-friendly, better captcha, fancy captcha, captcha alternative, jquery, jquery ui, drag, draggable, demo, retina, accessibility">
+        <meta name="description" content="A cool visual drag-and-drop captcha jQuery plugin. Mobile-friendly. Retina-ready. Accessible.">
+        
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
-        <link href="/css/visualcaptcha.css" media="all" rel="stylesheet" type="text/css" />
+        <!-- Mobile Specific Metas -->
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+        
+        <!-- CSS -->
+        <link href="css/demo.css" media="all" rel="stylesheet">
+
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css">
+
+        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+            <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+            <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+        <![endif]-->
+    </head>
+    <body>
+        <div id="wrap">
+            <div class="container">
+                <div class="row"> 
+                    <a href="/" target="_blank" id="logo"><img src="img/logo.png" alt="visualCaptcha"></a>
+                    <div class="pre-captcha-wrapper">
+                        <div class="captcha-wrapper">
+                            <h1>Fill in the form and submit it</h1>
+                            <form name="frm-sample" id="frm-sample" action="/try" method="post">
+                                <input type="text" class="form-control" placeholder="Name">
+                                <div id="status-message"></div>
+                                <div id="sample-captcha"></div>
+                                <a id="check-is-filled">Check if visualCaptcha is filled</a>
+                                <button type="submit" name="submit-bt" class="submit">Submit form</button>
+                            </form>
+                        </div>
+                    </div> 
+                    <div class="col-xs-12 links">
+                        <p class="txt-center">
+                            <a href="/" target="_blank"><b>visualCaptcha</b></a> by <a href="http://emotionloop.com/" target="_blank"><b>emotionLoop</b></a>
+                            <br />
+                            View other demos:
+                            <a href="http://node.demo.visualcaptcha.net/"><strong>Node.js</strong></a> |
+                            <a href="http://ruby.demo.visualcaptcha.net/"><strong>Ruby</strong></a> |
+                            <a href="http://meteor.demo.visualcaptcha.net/"><strong>Meteor</strong></a>
+                            <br />
+                            You can get it at <a href="https://github.com/emotionLoop/visualCaptcha" target="_blank"><b>GitHub</b></a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
 
         <script src="/js/jquery.min.js"></script>
         <script src="/js/visualcaptcha.jquery.js"></script>
         <script src="/js/main.js"></script>
-    </head>
-    <body>
-        <div class="container">
-            <h1>Fill in the form and submit it</h1>
-
-            <form name="frm-sample" id="frm-sample" action="/try" method="post">
-                <input type="text" class="form-control" placeholder="Name" />
-
-                <div class="captcha"></div>
-
-                <button type="submit" name="submit-bt" class="submit">Submit form</button>
-            </form>
-        </div>
     </body>
 </html>
+
--- public/js/main.js
+++ public/js/main.js
@@ -1,11 +1,43 @@
 ( function( window, $ ) {
     $( function() {
-        $( '.captcha' ).visualCaptcha( {
+        var captchaEl = $( '#sample-captcha' ).visualCaptcha({
             imgPath: '/img/',
             captcha: {
                 url: window.location.origin,
                 numberOfImages: 5
             }
         } );
+        var captcha = captchaEl.data( 'captcha' );
+
+
+
+        // Show an alert saying if visualCaptcha is filled or not
+        var _sayIsVisualCaptchaFilled = function() {
+            if ( captcha.getCaptchaData().valid ) {
+                window.alert( 'visualCaptcha is filled!' );
+            } else {
+                window.alert( 'visualCaptcha is NOT filled!' );
+            }
+        };
+
+        var statusEl = $( '#status-message' ),
+            queryString = window.location.search;
+        // Show success/error messages
+        if ( queryString.indexOf('status=noCaptcha') !== -1 ) {
+            statusEl.html('<div class="status"> <div class="icon-no"></div> <p>visualCaptcha was not started!</p> </div>');
+        } else if ( queryString.indexOf('status=validImage') !== -1 ) {
+            statusEl.html('<div class="status valid"> <div class="icon-yes"></div> <p>Image was valid!</p> </div>');
+        } else if ( queryString.indexOf('status=failedImage') !== -1 ) {
+            statusEl.html('<div class="status"> <div class="icon-no"></div> <p>Image was NOT valid!</p> </div>');
+        } else if ( queryString.indexOf('status=validAudio') !== -1 ) {
+            statusEl.html('<div class="status valid"> <div class="icon-yes"></div> <p>Accessibility answer was valid!</p> </div>');
+        } else if ( queryString.indexOf('status=failedAudio') !== -1 ) {
+            statusEl.html('<div class="status"> <div class="icon-no"></div> <p>Accessibility answer was NOT valid!</p> </div>');
+        } else if ( queryString.indexOf('status=failedPost') !== -1 ) {
+            statusEl.html('<div class="status"> <div class="icon-no"></div> <p>No visualCaptcha answer was given!</p> </div>');
+        }
+        
+        // Bind that function to the appropriate link
+        $( '#check-is-filled' ).click( _sayIsVisualCaptchaFilled );
     } );
 }( window, jQuery ) );
